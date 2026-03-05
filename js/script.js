f17dd7461af1513ac3f50ceda882e82a3f3ef0ef
Lewisameensimon <lewis.simon@rheincs.com>
2026-03-05 17:34:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-// reserved for future use
-console.log("H2Green website loaded");
+window.addEventListener("scroll",function(){
+
+const nav=document.querySelector("nav");
+
+if(nav){
+if(window.scrollY>50){
+nav.classList.add("scrolled");
+}else{
+nav.classList.remove("scrolled");
+}
+}
+
+});
+
+function toggleMenu(){
+const menu=document.getElementById("menu");
+if(menu){
+menu.classList.toggle("active");
+}
+}
+
+function toggleDropdown(e){
+
+if(window.innerWidth < 900){
+
+e.preventDefault();
+
+const parent=e.target.parentElement;
+
+parent.classList.toggle("open");
+
+}
+
+}
